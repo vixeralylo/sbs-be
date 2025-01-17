876cdc36172f89fc6e79019d7a1e4530374dd886
Sandy <sandy.taufik@it.sharingvision.com>
2025-01-17 14:58:23 +0700

    handle update price

--- delivery/update_product_delivery.go
+++ delivery/update_product_delivery.go
@@ -13,6 +13,7 @@ func (delivery *sbsDelivery) UpdateSbsProduct(c *gin.Context) {
 	sku := c.GetHeader("sku")
 	qty := c.GetHeader("qty")
 	hpp := c.GetHeader("hpp")
+	price := c.GetHeader("price")
 
 	// Convert string to float32
 	hppConvert, err := strconv.ParseFloat(hpp, 64)
@@ -21,7 +22,14 @@ func (delivery *sbsDelivery) UpdateSbsProduct(c *gin.Context) {
 		return
 	}
 
-	data := delivery.SbsUsecase.UpdateSbsProduct(c, sku, qty, hppConvert)
+	// Convert string to float32
+	priceConvert, err := strconv.ParseFloat(price, 64)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	data := delivery.SbsUsecase.UpdateSbsProduct(c, sku, qty, hppConvert, priceConvert)
 
 	if data.StatusCode >= 400 && data.StatusCode != http.StatusNotFound {
 		c.JSON(data.StatusCode, data)
--- repository/sbs_repository.go
+++ repository/sbs_repository.go
@@ -12,7 +12,7 @@ type SbsRepository interface {
 	Ping() string
 	GetSbsProduct(c context.Context) ([]entity.SbsProduct, error)
 	GetSbsProductById(c context.Context, sku string) ([]entity.SbsProduct, error)
-	UpdateSbsProduct(c context.Context, sku string, qty int, hpp float64) error
+	UpdateSbsProduct(c context.Context, sku string, qty int, hpp float64, price float64) error
 	DeductSbsProduct(c context.Context, sku string, qty int) error
 	AddSbsProduct(c context.Context, sku string, qty int) error
 	GetSo(c context.Context, filter dto.RequestSo) ([]entity.SbsSalesOrder, error)
