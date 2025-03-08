1786010bf4daf12755cf6ca903edd566b71b72e3
sandy <sandy.taufik93@gmail.com>
2025-03-08 15:00:20 +0700

    test

--- config/mysql.go
+++ config/mysql.go
@@ -15,7 +15,7 @@ import (
 var ErrRecordNotFound = errors.New("record not found")
 
 func ConnectMySQL() (*gorm.DB, error) {
-	connectionString := CONFIG["MYSQL_USER"] + ":" + CONFIG["MYSQL_PASS"] + "@tcp(" + CONFIG["MYSQL_HOST"] + ":" + CONFIG["MYSQL_PORT"] + ")/" + CONFIG["MYSQL_SCHEMA"] + "?parseTime=true&loc=Local"
+	connectionString := "root:root@tcp(host.docker.internal:3306)/sbs?parseTime=true&loc=Local"
 
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
