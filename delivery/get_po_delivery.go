9cb11a5f90804af9caf1d9c1f9c38d63155192fe
sandy <sandy.taufik93@gmail.com>
2025-03-09 21:04:55 +0700

    test

--- delivery/get_po_delivery.go
+++ delivery/get_po_delivery.go
@@ -1,6 +1,7 @@
 package delivery
 
 import (
+	"log"
 	"net/http"
 	"sbs-be/model/dto"
 
@@ -12,6 +13,10 @@ func (delivery *sbsDelivery) GetPo(c *gin.Context) {
 	start_date := c.GetHeader("start_date")
 	end_date := c.GetHeader("end_date")
 	is_not_payment := c.GetHeader("is_not_payment")
+
+	log.Println("start_date:", start_date)
+	log.Println("end_date:", end_date)
+	log.Println("is_not_payment:", is_not_payment)
 
 	filter := dto.RequestPo{
 		StartDate:    start_date,
