4cb5fd0da58dbdbe818bc3651e3754a19f3ffd4c
sandy <sandy.taufik93@gmail.com>
2025-05-09 20:23:39 +0700

    test

--- delivery/post_so_delivery.go
+++ delivery/post_so_delivery.go
@@ -56,7 +56,9 @@ func (delivery *sbsDelivery) PostSo(c *gin.Context) {
 		rows := xlsx.GetRows(sheet1Name)
 
 		for i := range rows {
+			fmt.PrintLn(1)
 			if i+1 < 3 || xlsx.GetCellValue(sheet1Name, fmt.Sprintf("B%d", i+1)) == "Canceled" {
+				fmt.PrintLn(2)
 				continue
 			}
 			
