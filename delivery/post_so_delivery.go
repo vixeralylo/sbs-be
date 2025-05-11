031ec5d3fba8c5cdaea40cb2d725fd2de30f0c33
sandy <sandy.taufik93@gmail.com>
2025-05-11 09:02:29 +0700

    debug

--- delivery/post_so_delivery.go
+++ delivery/post_so_delivery.go
@@ -56,10 +56,12 @@ func (delivery *sbsDelivery) PostSo(c *gin.Context) {
 		rows := xlsx.GetRows(sheet1Name)
 
 		for i := range rows {
+			fmt.Println("ORDER ID MASUK: ", xlsx.GetCellValue(sheet1Name, fmt.Sprintf("A%d", i+1)))
 			if i+1 < 3 || xlsx.GetCellValue(sheet1Name, fmt.Sprintf("B%d", i+1)) == "Canceled" {
 				continue
 			}
 			
+			fmt.Println("ORDER ID DIPROSES: ", xlsx.GetCellValue(sheet1Name, fmt.Sprintf("A%d", i+1)))
 			qty, err = strconv.Atoi(xlsx.GetCellValue(sheet1Name, fmt.Sprintf("J%d", i+1)))
 			if err != nil {
 				// ... handle error
